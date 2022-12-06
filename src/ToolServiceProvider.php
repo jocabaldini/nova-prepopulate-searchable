2d51e72a041382d62d47f2cfa89c665498be26ed
João Baldini <joaobaldini@exitlag.com>
2022-12-06 11:36:48 -0300

    Update namespace

--- src/ToolServiceProvider.php
+++ src/ToolServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace GeneaLabs\NovaPrepopulateSearchable;
+namespace JocaBaldini\NovaPrepopulateSearchable;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Nova\Events\ServingNova;
